f0796d34498c500e6e115e1d0d4571a74562e158
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-12-07 21:16:55 +0530

    pages updated

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [WebController::class, 'welcome']) -> name('welcome');
 
 Route::get('/home', function () {
     echo 'welcome';
 });
+
+Route::get('/home', [WebController::class, 'home']) -> name('home');
+Route::get('/about', [WebController::class, 'about'])-> name('about');
+Route::get('/contact', [WebController::class, 'contact'])-> name('contact');
